5cab71039d53834c71569a90df60a7b573d1c855
Ray Olivier <rayolivier@outlook.com>
2018-09-11 13:48:57 -0500

    mapped to make Posts

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
+import "./App.css";
 
-import Header from './Header/Header';
-import Compose from './Compose/Compose';
+import Post from "./Post/Post";
+import Header from "./Header/Header";
+import Compose from "./Compose/Compose";
+import axios from "axios";
+
+const BASE_URL = "https://practiceapi.devmountain.com/api";
 
 class App extends Component {
   constructor() {
@@ -13,26 +17,24 @@ class App extends Component {
       posts: []
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
-
+    axios.get(BASE_URL + "/posts").then(res => {
+      console.log("res: ", res);
+      this.setState({ posts: res.data });
+      console.log(this.state);
+    });
   }
 
-  updatePost() {
-  
-  }
+  updatePost() {}
 
-  deletePost() {
+  deletePost() {}
 
-  }
-
-  createPost() {
-
-  }
+  createPost() {}
 
   render() {
     const { posts } = this.state;
@@ -42,9 +44,10 @@ class App extends Component {
         <Header />
 
         <section className="App__content">
-
           <Compose />
-          
+          {posts.map(e => {
+            return <Post key={e.id} />;
+          })}
         </section>
       </div>
     );
